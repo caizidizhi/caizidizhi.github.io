a00b701efe85346e9bfeb8f793aed10ecd8f7c55
heihub <63719020+heihub@users.noreply.github.com>
2020-05-07 00:02:14 +0800

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -4,6 +4,15 @@
         document.getElementById("btnEnter").onclick = function() {
             window.open(enterUrl, "_blank");
         }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);
+
+(function(w) {
+    function enterLine() {
         let enterUrl = 'http://caiziav.github.io';
         document.getElementById("btnfabu").onclick = function() {
             window.open(enterUrl, "_blank");
@@ -12,5 +21,5 @@
     function init() {
         enterLine();
     }
-    window.addEventListener("DOMContentLoaded", init, !1);
+    window.addEventListener("DOMContentLoaded", init, !2);
 })(window);
